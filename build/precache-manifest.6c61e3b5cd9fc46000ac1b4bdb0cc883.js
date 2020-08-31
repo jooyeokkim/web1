eff13ac91b54fd2d265cda49b8b04521804e8830
jooyeokkim <67864645+jooyeokkim@users.noreply.github.com>
2020-08-31 15:38:15 +0900

    build

---
+++ build/precache-manifest.6c61e3b5cd9fc46000ac1b4bdb0cc883.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01a4525c341b62dde71297c9c069686e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "868479bf4166499cd6d3",
+    "url": "/static/css/main.50033459.chunk.css"
+  },
+  {
+    "revision": "28edbc318f06d6c0cf3f",
+    "url": "/static/js/2.0165973c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0165973c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "868479bf4166499cd6d3",
+    "url": "/static/js/main.efa1571d.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
